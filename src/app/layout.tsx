a27f9c79aecb1f0d0769d543ca121cf69048c268
Yaad Nahshon <ydnahshon@gmail.com>
2024-09-17 23:42:08 +0300

    add intro section and a portrait picture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import React from "react";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,13 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <body className={`${inter.className} bg-sky-50 text-gray-950 relative`}>
+    <body className={`${inter.className} bg-sky-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}>
     <div
-        className="bg-amber-50 absolute blur-[10rem] top-[-6rem] z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full sm:w-[68.75rem]">
+        className="bg-lime-50 absolute blur-[10rem] top-[-6rem] z-60 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full sm:w-[68.75rem]">
     </div>
     <div
-        className="bg-fuchsia-50 absolute blur-[10rem] top-[-1rem] z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]">
+        className="bg-amber-50 absolute blur-[10rem] top-[-1rem] z-60 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]">
     </div>
+    <Header />
     {children}
     </body>
     </html>
