76c1c7bf762c42d58ddaea36081c143a26dcd935
Yaad Nahshon <ydnahshon@gmail.com>
2024-09-18 22:18:49 +0300

    fix about component

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -16,7 +16,7 @@ export default function About() {
                 I’m an <span className="font-medium">enthusiastic software engineer</span>, self-learner, and excellent team player. I am excited to work with a talented team and contribute to the success of Intel’s mission to create world-changing technology that improves every person’s life on the planet. I <span className="underline">love</span> the feeling of finally figuring out a solution to a problem.
             </p>
             <p>
-                My core stack is <span className="font-medium">Python, Typescript, Jenkins, and Docker</span>. I'm also familiar with web development with React, Next.js and Node.js, and other DevOps tools.
+                My core stack is <span className="font-medium">Python, Typescript, Jenkins, and Docker</span>. I`m also familiar with web development with React, Next.js and Node.js, and other DevOps tools.
             </p>
         </motion.section>
     )
