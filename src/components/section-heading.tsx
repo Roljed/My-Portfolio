d2d9bdc2739abff560f4dd4c5fa7c46fd86fc887
Yaad Nahshon <ydnahshon@gmail.com>
2024-10-03 11:16:54 +0300

    style section heading with centered text

--- src/components/section-heading.tsx
+++ src/components/section-heading.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function SectionHeading({ title }: { title: string }) {
     return (
-        <h2 className="text-3xl font-medium capitalize mb-8">{title}</h2>
+        <h2 className="text-3xl font-medium capitalize mb-8 text-center">{title}</h2>
     )
 }
