c1f48a86fbd5f6d2eec67acb07c7e83195659298
Yaad Nahshon <ydnahshon@gmail.com>
2024-11-29 09:33:47 +0200

    add required fields for the contact component

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -18,14 +18,18 @@ export default function Contact() {
                         viewport={{ once: true }}
         >
             <SectionHeading title={"Contact"} />
-            <p className="text-gray-700 -mt-6">Please contact me directly at <a className="underline" href="mailto:ydnahshon@gmail.com">ydnahshon@gmail.com</a>, or trough this email form.</p>
+            <p  className="text-gray-700 -mt-6">Please contact me directly at <a className="underline" href="mailto:ydnahshon@gmail.com">ydnahshon@gmail.com</a>, or trough this email form.</p>
             <form className="mt-10 flex flex-col">
                 <input className="h-14 px-4 rounded-lg borderBlack"
                        type="email"
+                       required={true}
+                       maxLength={100}
                        placeholder="Your email"
                 />
                 <textarea className="h-52 my-3 p-4 rounded-lg borderBlack"
                           placeholder="Your message..."
+                          required={true}
+                          maxLength={500}
                 />
                 <button className="group h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all flex items-center justify-center gap-2 focus:scale-110 hover:scale-110 active:scale-105 bg-gray-950"
                         type="submit">Submit <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1"/></button>
---
+++ src/components/tools.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import SectionHeading from "@/components/section-heading";
+
+export default function Tools() {
+    return(
+        <section>
+            <SectionHeading title={"Tools"} />
+            <p>
+                My favorite tools
+            </p>
+        </section>
+
+
+    )
+}
