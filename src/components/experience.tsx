8dddc20c9c7dff5ccb286e22c2e9b74a6485f2ba
Yaad Nahshon <ydnahshon@gmail.com>
2024-12-07 06:29:08 +0200

    disable react hook rule for vertical timeline

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -21,6 +21,7 @@ export default function Experience() {
             <SectionHeading title={"My Experience"} />
             <VerticalTimeline lineColor="">
                 {experiencesData.map((item, index) => {
+                    // eslint-disable-next-line react-hooks/rules-of-hooks
                     const { ref, inView } = useInView({
                         triggerOnce: true,
                     });
