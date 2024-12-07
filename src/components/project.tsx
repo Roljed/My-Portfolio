c8acfd2c25337860bfef1201f4f23f7354be6bc7
Yaad Nahshon <ydnahshon@gmail.com>
2024-12-07 22:05:05 +0200

    fix: odd project margin and padding on small screens

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -32,8 +32,8 @@ export default function Project({
             className="group mb-3 sm:mb-8 last:mb-0"
         >
             <section
-                className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8">
-                <div className="py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] group-even:ml-[18rem]">
+                className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8 group-odd:pr-8">
+                <div className="py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] group-even:ml-[18rem] group-odd:mr-[18rem]">
                     <h3 className="text-2xl font-semibold">{title}</h3>
                     <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
                     <ul className="flex flex-wrap mt-4 gap-2">
