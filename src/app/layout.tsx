843e4b9f67f8baef8c9d4870576a04eac844c94e
Yaad Nahshon <ydnahshon@gmail.com>
2026-05-20 03:37:58 +0300

    feat: increase blob opacity in dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,10 @@ export default function RootLayout({
     <html lang="en" className="!scroll-smooth">
       <body className={`${inter.className} bg-sky-50 text-gray-950 dark:bg-stone-950 dark:text-stone-100 relative pt-28 sm:pt-36 overflow-x-hidden transition-colors`}>
         <div
-            className="bg-lime-50 dark:bg-lime-900/30 absolute blur-[10rem] top-[-6rem] z-60 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full sm:w-[68.75rem]">
+            className="bg-lime-50 dark:bg-lime-900/45 absolute blur-[10rem] top-[-6rem] z-60 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full sm:w-[68.75rem]">
         </div>
         <div
-            className="bg-amber-50 dark:bg-amber-900/35 absolute blur-[10rem] top-[-1rem] z-60 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]">
+            className="bg-amber-50 dark:bg-amber-900/50 absolute blur-[10rem] top-[-1rem] z-60 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]">
         </div>
         <ActiveSectionContextProvider>
           <Header />
